b5a979ecd85a74c946c0772c7e4fac1d507cb535
trito apriansyah <48681738+Tritoapriansyah@users.noreply.github.com>
2022-03-17 00:15:02 +0700

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -5,9 +5,9 @@ const chalk = require('chalk')
 global.self = false //jadiin true klo gk mau fitur bot lu di pke sama org lain
 
 // setting
-global.ownername ="RafliKunn"
-global.ownernumber = "62895410068780"
-global.botname = "RflBotz Ofc"
+global.ownername ="Trito"
+global.ownernumber = "6285888258313"
+global.botname = "Lolita ot"
 global.thumbnail = fs.readFileSync("./image/elaina.jpg") //sesuaikan dengan nama foto
 global.background = "https://telegra.ph/file/d4c05638fa7886a1d8060.jpg"
 global.lolkey = 'rafliofcheh' //ganti pakai apikey kalian
